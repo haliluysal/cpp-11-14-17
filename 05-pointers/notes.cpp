96f2b8f693012ec8217c679b550bb73c60bdd191
haliluysal <haliluysal@gmail.com>
2020-11-27 17:42:02 -0500

    pointers have discussed. - address operator (&) and dereference operators (*) explained - nullptr is introduced.

---
+++ 05-pointers/notes.cpp
@@ -0,0 +1,21 @@
+
+/*
+
+* points to another type
+* holds the memory address of another variable
+* used for indirect access to other variables
+* does "not" have be initialized
+* declared wit * operator
+*		int *p1;
+*		int *p2, *p3, p4 // p4 is normal integer not pointer
+* 
+* & is the address of operator
+* dereference operator "*"
+*		int x = 10;
+*		int *ptr = &x;
+*		*ptr = 5;
+*		int y = *ptr;
+* 
+* DO NOT try to write to or read from nullptr
+
+*/
---
+++ 05-pointers/pointers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+	int x = 10;
+
+	std::cout << &x << std::endl; // address of operator
+
+	int* ptr{ &x }; // void pointer can void to any type
+
+	std::cout << ptr << std::endl;
+
+	*ptr = 5;
+
+	std::cout << x << std::endl;
+
+	int y = *ptr;
+
+	std::cout << y << std::endl;
+
+
+
+	return 0;
+}
